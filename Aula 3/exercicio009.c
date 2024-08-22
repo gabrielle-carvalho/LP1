5050bc20a0fc830318eef6cb6f16a533fe5f2f14
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-22 08:22:49 -0300

    Create exercicio009.c

---
+++ Aula 3/exercicio009.c
@@ -0,0 +1,3 @@
+//Crie um programa em C que leia uma temperatura em graus Celsius e a converta para Fahrenheit. O programa deve exibir a mensagem "Frio" se a temperatura em Fahrenheit for menor que 50, e "Quente" se for maior ou igual a 50.
+//c=f-32/1.8
+//f=1.8*c+32
