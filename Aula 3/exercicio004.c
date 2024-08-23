3a5a52d79b7d39c2707f03bd028700ba45a70d35
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-22 21:22:32 -0300

    Update exercicio004.c

--- Aula 3/exercicio004.c
+++ Aula 3/exercicio004.c
@@ -1,1 +1,16 @@
-//Faça um algoritmo em C que leia um número inteiro e determine se ele é par ou ímpar.
+//3. Escreva um programa em C que leia um número inteiro e exiba todos os números 
+//ímpares de 1 até esse número.
+#include<stdio.h>
+int main(){
+    int num;
+
+  printf("Digite um numero: ");
+  scanf("%d", &num);
+
+for (int i = 1; i <= num; i++) {
+        if (num % i != 0) {
+            printf("%d ", i);
+        }
+}
+    
+}
