080f8fc336f41fb37c77964799ed349f84049136
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-22 21:40:26 -0300

    Update exercicio006.c

--- Aula 3/exercicio006.c
+++ Aula 3/exercicio006.c
@@ -1,1 +1,21 @@
-//Escreva um programa em C que leia dois números inteiros e exiba o maior deles. Caso sejam iguais, exiba uma mensagem informando que os números são iguais.
+//Escreva um programa em C que leia dois números inteiros e exiba o maior deles.
+//Caso sejam iguais, exiba uma mensagem informando que os números são iguais.
+#include<stdio.h>
+int main() {
+	int num,num2;
+
+	printf("Digite um numero: ");
+	scanf("%d", &num);
+	
+	printf("Digite um numero: ");
+	scanf("%d", &num2);
+	
+	if (num2==num){
+	    printf("os números são iguais.");
+	} else	if(num>num2){
+	    printf("%d é o maior", num);
+	} else{
+	    printf("%d é o maior", num2);
+	}
+	
+}
