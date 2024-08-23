05a6715f9e48cd2d1ba5bf6f44578eba8090ccf4
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-23 15:25:46 -0300

    Update exercicio-desafio.c

--- Aula 3/exercicio-desafio.c
+++ Aula 3/exercicio-desafio.c
@@ -1,1 +1,26 @@
-//Faça um programa que mostre o apenas o n-ésimo termo da série de Fibonacci. Esse programa deve receber como entrada um número inteiro n maior que 2 (considere que 0 e 1 são conhecidos, sendo eles os 2 primeiros números da série).
+//Faça um programa que mostre o apenas o n-ésimo termo da série de Fibonacci. Esse programa deve 
+//receber como entrada um número inteiro n maior que 2 (considere que 0 e 1 são conhecidos, sendo
+//eles os 2 primeiros números da série).
+#include<stdio.h>
+
+int main() {
+    
+    int num=0, newfibonacci=0, fibonacci=1, nextfibonacci=0;
+
+    printf("Digite um numero inteiro maior que 0: ");
+    scanf("%d", &num);
+    
+    for(int cont = 0; cont < num; ){
+    
+    if (cont == 1)
+    fibonacci = 1;
+    
+    nextfibonacci=fibonacci+newfibonacci;
+    
+    fibonacci=newfibonacci;
+    newfibonacci=nextfibonacci;
+    cont++;
+}
+    printf("Resultado = %d", nextfibonacci);
+    return 0;
+}
