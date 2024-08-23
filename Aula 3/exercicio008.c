1f1ac0d7ae6aed9fdca82a8aa3e581a5306c718a
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-23 11:16:11 -0300

    Update exercicio008.c

--- Aula 3/exercicio008.c
+++ Aula 3/exercicio008.c
@@ -1,1 +1,22 @@
-//Escreva um algoritmo em C que leia uma série de números inteiros (a entrada termina quando o número 0 é digitado) e exiba a soma desses números.
+//Escreva um algoritmo em C que leia uma série de números inteiros
+//(a entrada termina quando o número 0 é digitado) e exiba a soma desses números..
+#include <stdio.h>
+
+int main()
+{
+    int newnum, num;
+    
+    printf("Insira um numero: %d ", num);
+    scanf("%d", &num);
+    
+    while(num!=0){
+        newnum=num+newnum;
+        printf("Insira um numero: %d ", num);
+        scanf("%d", &num);
+    }   printf("%d", newnum);
+    
+ 
+
+
+    return 0;
+}
