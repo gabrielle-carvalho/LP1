c3638eded6452e62af108f4e21677a1a00b6be63
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-22 21:06:35 -0300

    Update exercicio002.c

--- Aula 3/exercicio002.c
+++ Aula 3/exercicio002.c
@@ -1,1 +1,22 @@
-//As maçãs custam R$ 0,30 cada se forem compradas menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra. [Marcelo Cendron, IFC]
+//As maçãs custam R$ 0,30 cada se forem compradas menos do que uma dúzia, 
+//e R$ 0,25 se forem compradas pelo menos doze. Escreva um programa que 
+//leia o número de maçãs compradas, calcule e escreva o valor total da compra. 
+
+#include<stdio.h>
+int main(){
+    int macas;
+  float total;
+
+  printf("Digite a quantidade de macas compradas: ");
+  scanf("%d", &macas);
+
+  if(macas<12){
+    total=macas*0.30;
+    printf("total = %.2f ", total);
+  }
+  else if (macas>=12){
+    total=macas*0.25;
+    printf("total = %.2f ", total);
+  }
+    
+}
