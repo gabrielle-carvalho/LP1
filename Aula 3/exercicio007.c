8a6c864d01a987c8ca54a4bbd57375218a4d5a08
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-22 21:45:18 -0300

    Update exercicio007.c

--- Aula 3/exercicio007.c
+++ Aula 3/exercicio007.c
@@ -1,1 +1,14 @@
 //Faça um programa em C que leia um número inteiro e exiba a tabuada desse número de 1 a 10.
+#include<stdio.h>
+int main(){
+    int newnum, num;
+
+  printf("Digite um numero: ");
+  scanf("%d", &num);
+
+for (int i = 1; i <= 10; i++) {
+            newnum=num*i;
+            printf("%d ", newnum);
+        }
+}
+    
