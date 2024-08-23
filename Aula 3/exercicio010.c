61fc6a47cc88e4514f87d8a297f40d7433336844
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-23 14:43:39 -0300

    Update exercicio010.c

--- Aula 3/exercicio010.c
+++ Aula 3/exercicio010.c
@@ -1,1 +1,19 @@
-//Faça um programa em C que leia uma sequência de números inteiros (a sequência termina quando o usuário digitar um número negativo) e exiba o maior número digitado.
+//Faça um programa em C que leia uma sequência de números inteiros (a sequência termina 
+//quando o usuário digitar um número negativo) e exiba o maior número digitado.
+#include<stdio.h>
+
+int main()
+{
+    int num, newnum=0;
+    while(num>=0){
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    if(num>0){
+      newnum=newnum+num;
+    }
+    
+
+    }
+    printf("Resultado = %d", newnum);
+    return 0;
+}
