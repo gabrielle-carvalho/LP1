48c9fb5440def8217c8419a8628ea2b59bd28d38
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-22 21:01:41 -0300

    Update exercicio001.c

--- Aula 3/exercicio001.c
+++ Aula 3/exercicio001.c
@@ -1,1 +1,18 @@
-//Faça um programa que implemente um menu onde o usurário deverá selecionar 1 ou 0. Caso seja entrado um número diferente, o programa deverá solicitar uma nova opção. [Marcelo Cendron, IFC]
+//Faça um programa que implemente um menu onde o usurário deverá selecionar 1 ou 0. Caso seja entrado um número diferente,
+//o programa deverá solicitar uma nova opção. [Marcelo Cendron, IFC]
+#include<stdio.h>
+int main(){
+  int caso;
+
+  printf("Digite 0 ou 1: ");
+  scanf("%d", &caso);
+
+  if(caso==0 || caso==1){
+  printf("fim");
+  }
+  else{
+  printf("Digite uma nova opção: ");
+  scanf("%d", &caso);  
+  }
+    
+}
