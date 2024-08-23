ffbb987f7b6a4b34ea7df09ab11f41bd5e74b96f
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-22 21:34:50 -0300

    Update exercicio005.c

--- Aula 3/exercicio005.c
+++ Aula 3/exercicio005.c
@@ -1,1 +1,15 @@
 //Crie um algoritmo em C que leia um número inteiro positivo e calcule o fatorial desse número.
+
+#include<stdio.h>
+int main() {
+	int num,fatorial=1;
+
+	printf("Digite um numero: ");
+	scanf("%d", &num);
+
+	for (int i = 1; i <= num;) {
+	    fatorial=fatorial*i;
+			
+			i++;
+		} printf("%d é o fatorial. ", fatorial);
+}
