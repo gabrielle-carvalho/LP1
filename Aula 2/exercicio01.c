faabda8172402959a3906e5ace2949710b563775
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-24 16:13:15 -0300

    Update exercicio01.c

--- Aula 2/exercicio01.c
+++ Aula 2/exercicio01.c
@@ -1,1 +1,14 @@
-//Fazer um programa em C que pergunta um valor em metros e imprime o correspondente em decímetros, centímetros e milímetros.
+//Fazer um programa em C que pergunta um valor em metros e imprime o correspondente em decímetros,
+//centímetros e milímetros.
+#include<stdio.h>
+    int main(){
+        float tamanho, decimetros=0, centimetros=0, milimetros=0;
+        printf("Digite o valor em metros: ");
+        scanf("%f", &tamanho);
+        
+        decimetros=tamanho*10;
+        centimetros=tamanho*100;
+        milimetros=tamanho*1000;
+        
+        printf("decimetros = %.2f, centimetros = %.2f, milimetros = %.2f.", decimetros, centimetros, milimetros);
+    }
