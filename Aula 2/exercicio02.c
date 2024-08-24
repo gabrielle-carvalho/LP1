2b21ba7fe9345a03ee1c96c876be73fcec073288
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-24 16:03:18 -0300

    Update exercicio02.c

--- Aula 2/exercicio02.c
+++ Aula 2/exercicio02.c
@@ -1,1 +1,14 @@
 //Fazer um programa em C que imprime a tabuada de um número lido do terminal
+#include <stdio.h>
+int main(){
+    int num, cont=0, tabuada=0;
+    
+    printf("Digite um número para ver sua tabuada:");
+    scanf("%d", &num);
+    
+    while(cont<=10){
+        tabuada=cont*num;
+        printf("%d X %d = %d\n", num, cont, tabuada);
+        cont++;
+    }
+}
