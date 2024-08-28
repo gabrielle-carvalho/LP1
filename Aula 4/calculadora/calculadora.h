adb591af1ba5d83481d8bf690ba78531a41336de
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-28 15:22:26 -0300

    Create calculadora.h

---
+++ Aula 4/calculadora/calculadora.h
@@ -0,0 +1,1 @@
+//Crie um arquivo "calculadora.h" e "calculadora.cpp" contendo, respectivamente, a declaração e a definição de funções para soma e subtração recebendo os parâmetros adequados. Dentro de um novo arquivo "main.cpp" inclua a calculadora e execute utilizando os comandos de compilação apropriados.
