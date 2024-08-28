20c3d5170821df0a14eef500d63fb289bfb89349
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-08-28 15:17:50 -0300

    Create questao1.c

---
+++ Aula 4/questao1.c
@@ -0,0 +1,2 @@
+//Tarefa: Escreva duas macros, MIN(x, y) e MAX(x, y), que retornem o mínimo e o máximo de dois valores x e y, respectivamente. Use essas macros em um programa para encontrar o mínimo e o máximo de diferentes pares de números.
+//DICA: Utilize o operador ternário
