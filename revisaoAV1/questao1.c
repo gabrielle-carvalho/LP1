68075c630d23f0e15a4453ec0d910514e27d2a05
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-09-06 12:06:16 -0300

    Create questao1.c

---
+++ revisaoAV1/questao1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int f(int v, i, int arr[]){
+
+    while(v>0){
+    arr[j]= v%10;
+    v=v/10;
+    // tam++;
+    }
+    for(j=0; j<tam; j++){
+    printf("%d", arr[j]);
+    }
+}
+//fazer função pra interver o array dps
+
+int main(){
+    int v, i, tam,i, arr[];
+    printf("Digite o tamanho de digitos do numero ");
+    scanf("%d", &tam);
+    printf("Digite o numero ");
+    scanf("%d", &v);
+
+}
