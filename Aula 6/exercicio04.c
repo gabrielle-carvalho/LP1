65cae46b9eb1ff6b2e3918b4d53aa8590f0abdcf
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-09-30 16:04:01 -0300

    Update exercicio04.c

--- Aula 6/exercicio04.c
+++ Aula 6/exercicio04.c
@@ -1,1 +1,37 @@
- // Escreva uma função areaRetangulo() que calcule a área de um retângulo..
+// // Defina um tipo Retângulo para retângulos. Represente um retângulo por seus pontos de 
+//extremidade inferior esquerdo e superior direito usando o tipo Ponto acima.
+#include<stdio.h>
+float altura=0, largura=0, area=0;
+
+typedef struct{
+    float x;
+    float y;
+}Ponto;
+
+typedef struct{
+    Ponto inferiorEsquerdo;
+    Ponto superiorDireito;
+} Retangulo;
+
+float calcularArea(Retangulo ret){
+    largura=ret.superiorDireito.x - ret.inferiorEsquerdo.x;
+    altura=ret.superiorDireito.y - ret.inferiorEsquerdo.y;
+
+    return largura*altura;
+}
+
+int main() {
+    Retangulo ret;
+
+    printf("Digite as coordenadas do ponto inferior esquerdo (x y): ");
+    scanf("%f %f", &ret.inferiorEsquerdo.x, &ret.inferiorEsquerdo.y);
+
+    printf("Digite as coordenadas do ponto superior direito (x y): ");
+    scanf("%f %f", &ret.superiorDireito.x, &ret.superiorDireito.y);
+    
+    float area = calcularArea(ret);
+    
+    printf("Área do retângulo: %.2f\n", area);
+
+    return 0;
+}
