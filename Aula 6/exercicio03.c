1f9b1fd6fdf967c5b184403a5e82730c29a13366
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-09-30 15:49:06 -0300

    Update exercicio03.c

--- Aula 6/exercicio03.c
+++ Aula 6/exercicio03.c
@@ -1,1 +1,28 @@
-// Defina um tipo Retângulo para retângulos. Represente um retângulo por seus pontos de extremidade inferior esquerdo e superior direito usando o tipo Ponto acima.
+// // Defina um tipo Retângulo para retângulos. Represente um retângulo por seus pontos de 
+//extremidade inferior esquerdo e superior direito usando o tipo Ponto acima.
+#include<stdio.h>
+typedef struct{
+    float x;
+    float y;
+}Ponto;
+
+typedef struct{
+    Ponto inferiorEsquerdo;
+    Ponto superiorDireito;
+} Retangulo;
+
+int main() {
+    Retangulo ret;
+
+    printf("Digite as coordenadas do ponto inferior esquerdo (x y): ");
+    scanf("%f %f", &ret.inferiorEsquerdo.x, &ret.inferiorEsquerdo.y);
+
+    printf("Digite as coordenadas do ponto superior direito (x y): ");
+    scanf("%f %f", &ret.superiorDireito.x, &ret.superiorDireito.y);
+
+    printf("\nRetângulo definido pelos pontos:\n");
+    printf("Inferior Esquerdo: (%.2f, %.2f)\n", ret.inferiorEsquerdo.x, ret.inferiorEsquerdo.y);
+    printf("Superior Direito: (%.2f, %.2f)\n", ret.superiorDireito.x, ret.superiorDireito.y);
+
+    return 0;
+}
