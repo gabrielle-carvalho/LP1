81824d8fc7636c27401f9a279485dc17366966dd
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-09-30 12:41:04 -0300

    Create exercicio02.c

---
+++ Aula 6/exercicio02.c
@@ -0,0 +1,33 @@
+// Crie uma struct que represente um aluno com os seguintes campos: nome, matricula (um número inteiro) e nota (um valor float). Escreva um programa que permita:
+//     Adicionar alunos a um array.
+//     Exibir todos os alunos com seus detalhes.
+//     Pesquisar um aluno pelo número de matrícula e exibir seus detalhes.
+#include<stdio.h>
+#define NOME_MAX_LEN 50
+
+typedef struct{
+char nome[nome_max_len+1];
+int matricula;
+float nota;
+} Aluno
+//     Defina a struct Aluno:
+//         A struct deve ter três campos: nome (uma string de caracteres), matricula (um número inteiro), e nota (um valor float).
+
+int adicionaAluno(){
+//         Adicionar Aluno: Uma função para adicionar um aluno a um array.
+}
+
+int exibeAluno(){
+  //         Exibir Alunos: Uma função para exibir todos os alunos no array.
+
+}
+int pesquisaAluno(){
+  //         Pesquisar Aluno: Uma função para pesquisar um aluno pelo número de matrícula.
+
+}
+
+int main(){
+//         Na função main, crie um array de alunos, adicione alguns alunos a ele e, em seguida, use as outras funções para exibir e pesquisar alunos.
+
+}
+
