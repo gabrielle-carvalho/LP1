cf93b19aa0dc38279ae34a61b9f8e1496c7b667f
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-10-25 12:35:37 -0300

    Create questao03.c

---
+++ Aula 10/questao03.c
@@ -0,0 +1,18 @@
+// Escreva uma função que tem um parâmetro inteiro com nome x de entrada. Aloque um vetor de inteiros com 5 vezes o tamanho. Inicialize o vetor com um valor diferente de zero.
+
+%%writefile teste.cpp
+// #include <cstdlib>
+#include <stdlib.h>
+#include<stdio.h>
+
+int * func (int x){
+    int n=x*5;
+    int * ptr = (int *) malloc(n*sizeof(int));
+    for(int i; i<n; ++i)
+      ptr[i]=1;
+
+    return ptr;
+}
+int main(){
+  int * v = func(5);
+}
