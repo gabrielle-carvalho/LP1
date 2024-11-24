7bbe621c14c76be69ab4be740dc754f9cbf775a2
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-11-24 19:20:01 -0300

    exemplo

---
+++ Aula 11/questao02.c
@@ -0,0 +1,13 @@
+// Partir string em dois
+typedef struct {
+    char *first_part;
+    char *second_part;
+} SplitResult;
+
+SplitResult split_string(char *str, const char delimitador)
+{
+  SplitResult result = {NULL, NULL};
+
+
+  return result;
+}
